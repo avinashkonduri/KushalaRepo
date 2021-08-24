4a929994260d4b45fb2f3901cb7e3acdb588ea56
avinashk <avinashk@logicmatter.com>
2021-08-24 08:51:05 +0530

    Implementation of Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,38 +6,145 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  Designation?: string;
+  Username?: string;
+  NoOfTeamMembers?: number;
+  TotalCostOfAllProjects?: number;
+  PendingTasks?: number;
+  UpComingProjects?: number;
+  ProjectCost?: number;
+  CurrentExpenditure?: number;
+  AvailableFunds?: number;
+  selectedDropdown?: string;
 
-  arrayData = [1,2,3,5];
-  arrayDataString?: string[];
-  arrayDataObj?: any[];
-  constructor(){
+  Clients?: string[];
+  Projects?: string[];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
 
-  }
-
+  date = new Date();
   ngOnInit() {
-    this.arrayDataString = ["Kushala", "Yamuna", "Hari","pooja"];
-    this.arrayDataObj = [
+    this.Designation = 'Team Leader';
+    this.Username = 'Scott Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+
+    this.Clients = [
+      'ABC Infotech Ltd.',
+      'DEF Software Solutions',
+      'GHI Industries',
+    ];
+
+    this.Projects = ['Project A', 'Project B', 'Project C', 'Project D'];
+    this.selectedDropdown = this.Projects[0];
+    for (var i = 2021; i >= 2015; i--) {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
       {
-        name: "Kushala",
-        role: "Developer",
-        location: "Hyderabad"
+        Region: 'East',
+        TeamMembersCount: 20,
+        TemporarilyUnavailableMembers: 4,
       },
       {
-        name: "Yamuna",
-        role: "Backend Developer",
-        location: "Warangal"
+        Region: 'West',
+        TeamMembersCount: 15,
+        TemporarilyUnavailableMembers: 8,
       },
       {
-        name: "Hari",
-        role: "Manager",
-        location: "Canada"
+        Region: 'South',
+        TeamMembersCount: 17,
+        TemporarilyUnavailableMembers: 1,
       },
       {
-        name: "pooja",
-        role: "HR",
-        location: "Hyderabad"
-      }
-    ]
+        Region: 'North',
+        TeamMembersCount: 15,
+        TemporarilyUnavailableMembers: 6,
+      },
+    ];
+
+    this.TeamMembers = [
+      {
+        Region: 'East',
+        Members: [
+          { ID: 1, Name: 'Ford', Status: 'Available' },
+          { ID: 2, Name: 'Miller', Status: 'Available' },
+          { ID: 3, Name: 'Jones', Status: 'Busy' },
+          { ID: 4, Name: 'James', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'West',
+        Members: [
+          { ID: 5, Name: 'Anna', Status: 'Available' },
+          { ID: 6, Name: 'Arun', Status: 'Available' },
+          { ID: 7, Name: 'Varun', Status: 'Busy' },
+          { ID: 8, Name: 'Jasmine', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'South',
+        Members: [
+          { ID: 9, Name: 'Krishna', Status: 'Available' },
+          { ID: 10, Name: 'Mohan', Status: 'Available' },
+          { ID: 11, Name: 'Raju', Status: 'Busy' },
+          { ID: 12, Name: 'Farooq', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'North',
+        Members: [
+          { ID: 13, Name: 'Jacob', Status: 'Available' },
+          { ID: 14, Name: 'Smith', Status: 'Available' },
+          { ID: 15, Name: 'Jones', Status: 'Busy' },
+          { ID: 16, Name: 'James', Status: 'Busy' },
+        ],
+      },
+    ];
   }
 
+  onProjectChange($event: any) {
+    this.selectedDropdown = $event.target.value;
+    // if ($event.target.innerHTML == 'Project A') {
+    //   this.ProjectCost = 2113507;
+    //   this.CurrentExpenditure = 96788;
+    //   this.AvailableFunds = 52436;
+    // } else if ($event.target.innerHTML == 'Project B') {
+    //   this.ProjectCost = 88923;
+    //   this.CurrentExpenditure = 22450;
+    //   this.AvailableFunds = 2640;
+    // } else if ($event.target.innerHTML == 'Project C') {
+    //   this.ProjectCost = 662183;
+    //   this.CurrentExpenditure = 7721;
+    //   this.AvailableFunds = 9811;
+    // } else if ($event.target.innerHTML == 'Project D') {
+    //   this.ProjectCost = 928431;
+    //   this.CurrentExpenditure = 562;
+    //   this.AvailableFunds = 883;
+    // }
+    if ($event.target.value == 'Project A') {
+      this.ProjectCost = 2113507;
+      this.CurrentExpenditure = 96788;
+      this.AvailableFunds = 52436;
+    } else if ($event.target.value == 'Project B') {
+      this.ProjectCost = 88923;
+      this.CurrentExpenditure = 22450;
+      this.AvailableFunds = 2640;
+    } else if ($event.target.value == 'Project C') {
+      this.ProjectCost = 662183;
+      this.CurrentExpenditure = 7721;
+      this.AvailableFunds = 9811;
+    } else if ($event.target.value == 'Project D') {
+      this.ProjectCost = 928431;
+      this.CurrentExpenditure = 562;
+      this.AvailableFunds = 883;
+    }
+  }
 }
